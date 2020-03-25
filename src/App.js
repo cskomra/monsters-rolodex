b9df4c080c560eb5bddc74d6da2be06e7acf5c12
Connie Skomra <connie@cskomra.com>
2020-03-25 15:51:54 -0400

    Add monsters json list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>response.json())
+    .then(users => this.setState({ monsters: users}))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        }
+      </div>
+    );
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Hello, my name is Connie.
         </p>
         <a
           className="App-link"
@@ -21,6 +48,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
